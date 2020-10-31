82fd69cc407605d7c778313f179d3a13493d7b2d
Mahak Mandlecha <mahakmandlecha04@gmail.com>
2020-10-31 11:39:13 +0530

    updated onboarding

--- plasma_donor/lib/data/data.dart
+++ plasma_donor/lib/data/data.dart
@@ -1,50 +1,46 @@
 import 'package:flutter/material.dart';
 
-
-class SliderModel{
-
+class SliderModel {
   String imageAssetPath;
   String title;
   String desc;
 
-  SliderModel({this.imageAssetPath,this.title,this.desc});
+  SliderModel({this.imageAssetPath, this.title, this.desc});
 
-  void setImageAssetPath(String getImageAssetPath){
+  void setImageAssetPath(String getImageAssetPath) {
     imageAssetPath = getImageAssetPath;
   }
 
-  void setTitle(String getTitle){
+  void setTitle(String getTitle) {
     title = getTitle;
   }
 
-  void setDesc(String getDesc){
+  void setDesc(String getDesc) {
     desc = getDesc;
   }
 
-  String getImageAssetPath(){
+  String getImageAssetPath() {
     return imageAssetPath;
   }
 
-  String getTitle(){
+  String getTitle() {
     return title;
   }
 
-  String getDesc(){
+  String getDesc() {
     return desc;
   }
-
 }
 
-
-List<SliderModel> getSlides(){
-
+List<SliderModel> getSlides() {
   List<SliderModel> slides = new List<SliderModel>();
   SliderModel sliderModel = new SliderModel();
 
   //1
-  sliderModel.setDesc("Just recovered from Covid-19 😊 or having Covid 19 😟 No worries");
+  sliderModel.setDesc(
+      "Just recovered from Covid-19 😊 or having Covid 19 😟 No worries");
   sliderModel.setTitle("Donate Plasma Save Life");
-  sliderModel.setImageAssetPath("assets/illustration.png");
+  sliderModel.setImageAssetPath("assets/illustration1.PNG");
   slides.add(sliderModel);
 
   sliderModel = new SliderModel();
@@ -52,15 +48,16 @@ List<SliderModel> getSlides(){
   //2
   sliderModel.setDesc("Having Covid-19? Need a plasma doner? 🔍 Donor here");
   sliderModel.setTitle("Find a donor");
-  sliderModel.setImageAssetPath("assets/illustration3.png");
+  sliderModel.setImageAssetPath("assets/illustration2.png");
   slides.add(sliderModel);
 
   sliderModel = new SliderModel();
 
   //3
-  sliderModel.setDesc("Congratulations on recovering from Covid-19. Help others to recover");
+  sliderModel.setDesc(
+      "Congratulations on recovering from Covid-19. Help others to recover.");
   sliderModel.setTitle("Become the Donor");
-  sliderModel.setImageAssetPath("assets/illustration3.png");
+  sliderModel.setImageAssetPath("assets/illustration3.PNG");
   slides.add(sliderModel);
 
   sliderModel = new SliderModel();
